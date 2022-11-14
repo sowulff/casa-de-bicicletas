f21e8c3edfe74a54560d283cda15a8e6c9e74051
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-11-14 15:17:08 +0100

    about-us responsive

--- resources/js/Pages/About/About.js
+++ resources/js/Pages/About/About.js
@@ -6,22 +6,25 @@ export default function About() {
     return (
         <div className={styles.about}>
             <NavBar />
-            <div className={styles.fullPageContainer}>
-                <div className={styles.imageContainer}>
-                    <img src={"assets/galleri/19.JPG"} />
-                    <div className={styles.textContainer}>
-                        <h1>Om oss</h1>
-                        <p>
-                            Jag heter Jonas och blablabla jag heter Molle och
-                            jag fjgngfgfng jkgns . Jag heter Jonas och blablabla
-                            jag heter Molle och jag fjgngfgfng jkgns . Jag heter
-                            Jonas och blablabla jag heter Molle och jag
-                            fjgngfgfng jkgns . Jag heter Jonas och blablabla jag
-                            heter Molle och jag fjgngfgfng jkgns . Jag heter
-                            Jonas och blablabla jag heter Molle och jag
-                            fjgngfgfng jkgns . Jag heter Jonas och blablabla jag
-                            heter Molle och jag fjgngfgfng jkgns{" "}
-                        </p>
+            <div className={styles.wrapper}>
+                <div className={styles.fullPageContainer}>
+                    <div className={styles.imageContainer}>
+                        <img src={"assets/galleri/19.JPG"} />
+                        <div className={styles.textContainer}>
+                            <h1>Om oss</h1>
+                            <p>
+                                Jag heter Jonas och blablabla jag heter Molle
+                                och jag fjgngfgfng jkgns . Jag heter Jonas och
+                                blablabla jag heter Molle och jag fjgngfgfng
+                                jkgns . Jag heter Jonas och blablabla jag heter
+                                Molle och jag fjgngfgfng jkgns . Jag heter Jonas
+                                och blablabla jag heter Molle och jag fjgngfgfng
+                                jkgns . Jag heter Jonas och blablabla jag heter
+                                Molle och jag fjgngfgfng jkgns . Jag heter Jonas
+                                och blablabla jag heter Molle och jag fjgngfgfng
+                                jkgns{" "}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
