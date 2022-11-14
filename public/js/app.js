092e78ce9cf0e0a0c5ece36535b26770a6dcd2d4
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-11-14 15:02:40 +0100

    about

--- public/js/app.js
+++ public/js/app.js
@@ -5284,13 +5284,21 @@ __webpack_require__.r(__webpack_exports__);
 function About() {
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
     className: _about_module_css__WEBPACK_IMPORTED_MODULE_0__["default"].about,
-    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_components_NavBar_NavBar__WEBPACK_IMPORTED_MODULE_2__["default"], {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
-      className: _about_module_css__WEBPACK_IMPORTED_MODULE_0__["default"].imageContainer,
-      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("img", {
-        src: "assets/galleri/19.JPG"
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("h3", {
-        children: ["Om ossJag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns", " "]
-      })]
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_components_NavBar_NavBar__WEBPACK_IMPORTED_MODULE_2__["default"], {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+      className: _about_module_css__WEBPACK_IMPORTED_MODULE_0__["default"].fullPageContainer,
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+        className: _about_module_css__WEBPACK_IMPORTED_MODULE_0__["default"].imageContainer,
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("img", {
+          src: "assets/galleri/19.JPG"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+          className: _about_module_css__WEBPACK_IMPORTED_MODULE_0__["default"].textContainer,
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h1", {
+            children: "Om oss"
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("p", {
+            children: ["Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter Molle och jag fjgngfgfng jkgns", " "]
+          })]
+        })]
+      })
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_components_Footer_Footer__WEBPACK_IMPORTED_MODULE_1__["default"], {})]
   });
 }
@@ -6712,11 +6720,13 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n    src: url(\"/../assets/fonts/Roboto-Thin.ttf\");\n    font-family: Roboto-Thin;\n}\n\n.tVxuWpOLNj1lRMxlQpYkng\\=\\= {\n    font-family: Roboto-Thin;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= {\n    display: flex;\n    justify-content: center;\n    align-items: center;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= img {\n    max-width: 55%;\n    height: auto;\n    border-radius: 0px 0 50px 0;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= h3 {\n    padding: 0 2rem;\n}\n@media only screen and (max-width: 850px) {\n    .ujP2vLmV4SjdR9BW2IAzZA\\=\\= {\n        flex-direction: column;\n        gap: 2rem;\n    }\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n    src: url(\"/../assets/fonts/Roboto-Thin.ttf\");\n    font-family: Roboto-Thin;\n}\n\n.tVxuWpOLNj1lRMxlQpYkng\\=\\= {\n    font-family: Roboto-Thin;\n}\n\n.fW8DKrA6lh6i8G7tRFbQXQ\\=\\= {\n    border: 2px solid #aeaaa245;\n    border-radius: 50px 0px 0 50px;\n    border-left: none;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= {\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    height: 90%;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= img {\n    max-width: 55%;\n\n    border-radius: 50px 0 50px 0;\n    -o-object-fit: cover;\n       object-fit: cover;\n}\n\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= h1 {\n    padding-bottom: 1rem;\n}\n.ujP2vLmV4SjdR9BW2IAzZA\\=\\= p,\nh1 {\n    padding: 0 2rem;\n}\n\n.rw7KynDNZdxtv3Zt\\+nDYDw\\=\\= {\n    display: flex;\n    flex-direction: column;\n}\n\n@media only screen and (max-width: 850px) {\n    .ujP2vLmV4SjdR9BW2IAzZA\\=\\= {\n        flex-direction: column;\n        gap: 2rem;\n    }\n}\n", ""]);
 // Exports
 ___CSS_LOADER_EXPORT___.locals = {
 	"about": "tVxuWpOLNj1lRMxlQpYkng==",
-	"imageContainer": "ujP2vLmV4SjdR9BW2IAzZA=="
+	"fullPageContainer": "fW8DKrA6lh6i8G7tRFbQXQ==",
+	"imageContainer": "ujP2vLmV4SjdR9BW2IAzZA==",
+	"textContainer": "rw7KynDNZdxtv3Zt+nDYDw=="
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -6902,7 +6912,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n    src: url(\"/../assets/fonts/Roboto-Thin.ttf\");\n    font-family: Roboto-Thin;\n}\n\n@font-face {\n    src: url(\"/../assets/fonts/JuliusSansOne-Regular.ttf\");\n    font-family: Julius;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= {\n    display: flex;\n    justify-content: center;\n    flex-direction: column;\n    align-items: center;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= h1 {\n    font-size: 1.5rem !important;\n    font-family: Roboto-Thin;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= p {\n    font-size: 1rem;\n    font-family: Roboto-Thin;\n}\n\n.x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n    display: flex;\n    flex-direction: row;\n    align-items: center;\n    border: 2px solid #aeaaa245;\n    border-radius: 50px 50px 0 50px;\n    width: 60%;\n    font-family: Roboto-Thin;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= img {\n    width: 100%;\n    border-radius: 0px 50px 0 0px;\n}\n\n.oIFLP0uZdmjXHyD2aosGBA\\=\\= {\n    width: 40%;\n    display: flex;\n    align-items: center;\n    height: 100%;\n}\n\n.wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n    width: 60%;\n    display: flex;\n    justify-content: right;\n    align-items: right;\n    flex-direction: column;\n    gap: 1rem;\n    padding: 2rem;\n    text-align: right;\n}\n\n@media only screen and (max-width: 1000px) {\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n        display: flex;\n        justify-content: center;\n        align-items: center;\n        text-align: left;\n        margin: 0 !important;\n        width: 80% !important;\n    }\n\n    .wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n        padding: 1rem;\n        gap: 5px;\n        margin-left: 10px;\n    }\n\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= h1 {\n        font-size: 18px;\n    }\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= p {\n        font-size: 16px;\n    }\n\n    /* .imageContainer {\n        width: 40%;\n        display: flex;\n        align-items: center;\n        height: 100%;\n    } */\n}\n@media only screen and (max-width: 600px) {\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n        margin: 2rem;\n    }\n    .oIFLP0uZdmjXHyD2aosGBA\\=\\= {\n        display: none;\n    }\n\n    .wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n        padding: 2rem;\n        width: 80%;\n    }\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n    src: url(\"/../assets/fonts/Roboto-Thin.ttf\");\n    font-family: Roboto-Thin;\n}\n\n@font-face {\n    src: url(\"/../assets/fonts/JuliusSansOne-Regular.ttf\");\n    font-family: Julius;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= {\n    display: flex;\n    justify-content: center;\n    flex-direction: column;\n    align-items: center;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= h1 {\n    font-size: 1.5rem !important;\n    font-family: Roboto-Thin;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= p {\n    font-size: 1rem;\n    font-family: Roboto-Thin;\n}\n\n.x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n    display: flex;\n    flex-direction: row;\n    align-items: center;\n    border: 2px solid #aeaaa245;\n    border-radius: 50px 50px 0 50px;\n    width: 60%;\n    font-family: Roboto-Thin;\n}\n\n.M8LJ3yDECOdkQ27zqYiceg\\=\\= img {\n    width: 100%;\n    border-radius: 0px 50px 0 0px;\n}\n\n.oIFLP0uZdmjXHyD2aosGBA\\=\\= {\n    width: 40%;\n    display: flex;\n    align-items: center;\n    height: 100%;\n}\n\n.wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n    width: 60%;\n    display: flex;\n    justify-content: left;\n    align-items: left;\n    flex-direction: column;\n    gap: 1rem;\n    padding: 2rem;\n}\n\n@media only screen and (max-width: 1000px) {\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n        display: flex;\n        justify-content: center;\n        align-items: center;\n        text-align: left;\n        margin: 0 !important;\n        width: 80% !important;\n    }\n\n    .wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n        padding: 1rem;\n        gap: 5px;\n        margin-left: 10px;\n    }\n\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= h1 {\n        font-size: 18px;\n    }\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= p {\n        font-size: 16px;\n    }\n\n    /* .imageContainer {\n        width: 40%;\n        display: flex;\n        align-items: center;\n        height: 100%;\n    } */\n}\n@media only screen and (max-width: 600px) {\n    .x-H8MV4cNjxxbxIeI0Gmiw\\=\\= {\n        margin: 2rem;\n    }\n    .oIFLP0uZdmjXHyD2aosGBA\\=\\= {\n        display: none;\n    }\n\n    .wlJuN67ILQJ2-IWokDJZvg\\=\\= {\n        padding: 2rem;\n        width: 80%;\n    }\n}\n", ""]);
 // Exports
 ___CSS_LOADER_EXPORT___.locals = {
 	"wrapper": "M8LJ3yDECOdkQ27zqYiceg==",
--- resources/js/Pages/About/About.js
+++ resources/js/Pages/About/About.js
@@ -6,18 +6,24 @@ export default function About() {
     return (
         <div className={styles.about}>
             <NavBar />
-            <div className={styles.imageContainer}>
-                <img src={"assets/galleri/19.JPG"} />
-                <h3>
-                    Om ossJag heter Jonas och blablabla jag heter Molle och jag
-                    fjgngfgfng jkgns . Jag heter Jonas och blablabla jag heter
-                    Molle och jag fjgngfgfng jkgns . Jag heter Jonas och
-                    blablabla jag heter Molle och jag fjgngfgfng jkgns . Jag
-                    heter Jonas och blablabla jag heter Molle och jag fjgngfgfng
-                    jkgns . Jag heter Jonas och blablabla jag heter Molle och
-                    jag fjgngfgfng jkgns . Jag heter Jonas och blablabla jag
-                    heter Molle och jag fjgngfgfng jkgns{" "}
-                </h3>
+            <div className={styles.fullPageContainer}>
+                <div className={styles.imageContainer}>
+                    <img src={"assets/galleri/19.JPG"} />
+                    <div className={styles.textContainer}>
+                        <h1>Om oss</h1>
+                        <p>
+                            Jag heter Jonas och blablabla jag heter Molle och
+                            jag fjgngfgfng jkgns . Jag heter Jonas och blablabla
+                            jag heter Molle och jag fjgngfgfng jkgns . Jag heter
+                            Jonas och blablabla jag heter Molle och jag
+                            fjgngfgfng jkgns . Jag heter Jonas och blablabla jag
+                            heter Molle och jag fjgngfgfng jkgns . Jag heter
+                            Jonas och blablabla jag heter Molle och jag
+                            fjgngfgfng jkgns . Jag heter Jonas och blablabla jag
+                            heter Molle och jag fjgngfgfng jkgns{" "}
+                        </p>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
