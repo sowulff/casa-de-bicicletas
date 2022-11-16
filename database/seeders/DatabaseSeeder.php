cd481ffdb8e9f92235db624afe00ddb63d1ebc87
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-11-16 12:20:09 +0100

    new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         DB::table('rooms')->insert(
             [
                 'name' => "Lägenhet",
-                'image' => "https://live.staticflickr.com/65535/52483900458_2d90d6deda.jpg",
+                // 'image' => "https://live.staticflickr.com/65535/52483900458_2d90d6deda.jpg",
+                'image' => "https://www.decorilla.com/online-decorating/wp-content/uploads/2019/01/luxutry-apartment-design-online-feature.jpg",
                 'price' => 69,
                 'description' => "Lägenhet med fullt utrustat kök, vardagsrum, matplats, två badrum och fyra sovplatser"
             ],
@@ -29,7 +30,8 @@ class DatabaseSeeder extends Seeder
         DB::table('rooms')->insert(
             [
                 'name' => "Dubbelrum",
-                'image' => "https://live.staticflickr.com/65535/52483815710_81e5b4c64f.jpg",
+                // 'image' => "https://live.staticflickr.com/65535/52483815710_81e5b4c64f.jpg",
+                'image' => "https://images.unsplash.com/photo-1566665797739-1674de7a421a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmVkcm9vbXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60",
                 'price' => 40,
                 'description' => "Rum med dubbelsäng och stort badrum. Rummet har både fläkt och aircondition."
             ],
@@ -37,7 +39,8 @@ class DatabaseSeeder extends Seeder
         DB::table('rooms')->insert(
             [
                 'name' => "Ställplats",
-                'image' => "https://live.staticflickr.com/65535/52483900488_30f5445a8f_w.jpg",
+                // 'image' => "https://live.staticflickr.com/65535/52483900488_30f5445a8f_w.jpg",
+                'image' => "https://images.prismic.io/siestacampers/1fa39f07-9efe-4c5d-b4e2-2058c8c18ed9_portugal-campervan-hire-kombi-classic-van-small.jpg?auto=compress,format",
                 'price' => 17,
                 'description' => "Ställplats för bil, campervan, husvagn eller husbil med tillgång till el och vatten."
             ],
@@ -45,7 +48,8 @@ class DatabaseSeeder extends Seeder
         DB::table('rooms')->insert(
             [
                 'name' => "Fembäddsrum",
-                'image' => "https://live.staticflickr.com/65535/52483352471_e6203e41f5_w.jpg",
+                // 'image' => "https://live.staticflickr.com/65535/52483352471_e6203e41f5_w.jpg",
+                'image' => "https://images.unsplash.com/photo-1636917754428-60ece5e521df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJ1bmslMjBiZWR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
                 'price' => 60,
                 'description' => "Sovsal med fem sängplatser, badrum. Tillgång till fullt utrustat kök och pool. "
             ],
